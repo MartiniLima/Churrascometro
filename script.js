7fabbf1e839b50640b0bde1fac3898d11d00cb66
Fabiana Martini <martinifabi996@gmail.com>
2021-11-03 21:43:29 -0300

    files

---
+++ script.js
@@ -0,0 +1,55 @@
+
+//Cerveja - 1200ml por pessoas --  se + de 6hras 2000ml
+//Refri/agua - 1000ml por pessoa -- se + de 6hras 1500ml
+
+//Crianças valem por meia pessoa
+
+let inAdultos = document.getElementById("adultos")
+let inCriancas = document.getElementById("criancas")
+let inDuracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calcular(){
+    console.log("calculando...")
+
+    let adultos = inAdultos.value
+    let criancas = inCriancas.value
+    let duracao = inDuracao.value
+
+    let qtdCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas)
+
+    let qtdBebida = bebidaPP(duracao) * adultos +(bebidaPP(duracao) / 2 * criancas)
+
+    let qtdCerveja = cervejaPP(duracao) * adultos
+
+
+    resultado.innerHTML = `<p>${qtdCarne}g de Carne</p>`
+    resultado.innerHTML += `<b><p>${qtdBebida}ml de Agua, Refri e/ou Suco</p></b>`
+    resultado.innerHTML += `<p>${qtdCerveja}ml de Cerveja</p>`
+}
+
+function carnePP(duracao){
+    if(duracao >= 6){
+        return  650
+    } else{
+        return 400
+    }
+}
+
+function bebidaPP(duracao){
+    if(duracao >= 6){
+        return 1500
+    }
+    else{
+        return 1000
+    }
+}
+
+function cervejaPP(duracao){
+    if(duracao >= 6){
+        return 2000
+    }else{
+        return 1200
+    }
+}
